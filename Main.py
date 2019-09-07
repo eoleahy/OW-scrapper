e3235801389ae2cec06f523e8954c9ab75c1590b
eoleahy <eoin.97@live.ie>
2019-09-07 17:32:42 +0100

    added timer

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
-
 import os, sys
-from threading import Timer
-from _thread import *
 from overwatch import Overwatch
+import time
 
 import pygame
 
@@ -11,18 +9,15 @@ image_path = "src/icons/"
 font_size=180
 FULL_SCREEN = 0
 
+timer_length = 300 #5 minutes
+
+
 def main():
 
     #                   platform,region,account
     overwatch=Overwatch("","","")
 
-    elo = overwatch.get_rating()
-    tank_rating=elo[0]
-    damage_rating=elo[1]
-    support_rating=elo[2]
-    average_rating=elo[3]
-
-    overwatch.profile_to_json()
+    #overwatch.profile_to_json()
 
     pygame.init()
 
@@ -36,13 +31,19 @@ def main():
     else:
         screen = pygame.display.set_mode(size)
 
-    #Loop that renders images
+    #Loop that fetches data and renders images
 
 
 
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+
+        elo = overwatch.get_rating()
+        tank_rating=elo[0]
+        damage_rating=elo[1]
+        support_rating=elo[2]
+        average_rating=elo[3]
 
         tank_rank = determine_rank(tank_rating)
         damage_rank = determine_rank(damage_rating)
@@ -54,9 +55,9 @@ def main():
         support_text = font.render(str(support_rating)+"-Support",True,[255,255,255])
         text = font.render(str(average_rating)+"-Average",True,[255,255,255])
 
-        tank_image = pygame.image.load(image_path + average_rank + ".png")
-        damage_image = pygame.image.load(image_path + average_rank + ".png")
-        support_image = pygame.image.load(image_path + average_rank + ".png")
+        tank_image = pygame.image.load(image_path + tank_rank + ".png")
+        damage_image = pygame.image.load(image_path + damage_rank + ".png")
+        support_image = pygame.image.load(image_path + support_rank + ".png")
         rank_image = pygame.image.load(image_path + average_rank + ".png")
 
         tank_rect = tank_image.get_rect()
@@ -103,6 +104,8 @@ def main():
         screen.blit(text,text_rect)
 
         pygame.display.flip()
+
+        time.sleep(timer_length)
 
 def determine_rank(elo):
 
